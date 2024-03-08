bc675cb2c6d4f7f8789b98a2c9bbab8ee5efcda8
FastWolf-051 <138472083+FastWolf-051@users.noreply.github.com>
2024-03-08 16:53:35 +0300

    moved Test.cs

---
+++ Source/Test.cs
@@ -0,0 +1,26 @@
+﻿/* #------------------ License ------------------#
+*  #---------- Created By FastDevil -------------#
+*  #--------- You can edit this file ------------#
+*  #------ This file is a example how to --------#
+*  #------------ Use this library ---------------#
+*  #---------------------------------------------#
+*  #-------- ©Copyright FastDevil 2024 ----------#
+*  #---------------------------------------------#
+*/
+#pragma warning disable
+
+using FastDevil.Debug;
+using FastDevil.Project;
+
+sealed class Run{
+    private static void Main(){
+        App.Exec_Config();
+        // Create Warning
+        CRT_Warn CRTW = new CRT_Warn(-10318,"Reading invaild memory address");
+        // Create Error
+        CRT_Error CRTE = new CRT_Error(271,"Failed try to delete file",5);
+        // Create message
+        CRT_Msg CRTM = new CRT_Msg("Chill,this not needs atention");
+        App.WaitKeyPress();
+    }
+}
